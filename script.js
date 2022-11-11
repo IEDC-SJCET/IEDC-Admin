e3b42f00f1ce0e25e4eae658f1c37fe72867df2e
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2022-11-11 11:52:05 +0000

    done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+import { getFirestore, getDocs, addDoc, collection, orderBy, query } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
+import { getStorage, ref, uploadBytes  } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCNbmkHVo6YAOk69h9OgMGbQJBUlW5xz4c",
@@ -13,18 +14,40 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
+const storageRef = ref(storage, 'some-child');
+
 var currentdate = new Date().toLocaleString();
 
+const name = document.getElementById('name').value;
+const dateS= document.getElementById('dateS').value;
+const link= document.getElementById('link').value;
+const dateE= document.getElementById('dateE').value;
+const message= document.getElementById('message').value;
+// const photo= document.getElementById('photo').value;
+const file = document.getElementById("photo").files[0];
+const time= currentdate;
+  
 document.getElementById("form").onsubmit = async function (e) {
     
     e.preventDefault();
-     const docRef = await addDoc(collection(db, "EVENT"), {
-        name: document.getElementById('name').value,
-        dateS: document.getElementById('dateS').value,
-        link: document.getElementById('link').value,
-        dateE: document.getElementById('dateE').value,
-        message: document.getElementById('message').value,
-        photo: document.getElementById('photo').value,
+     
+
+    uploadBytes(storageRef, file).then((snapshot) => {
+    console.log('file uploaded');
+    afterPic()
+  });
+}
+
+function afterPic(){
+  const docRef = addDoc(collection(db, "EVENT"), {
+        name: name,
+        dateS: dateS,
+        link: link,
+        dateE: dateE,
+        message:message,
         time: currentdate
     });
+
+    
 }
