6d7c447c8e01f83857e7d06347bf718d2c685f25
RajatSandeep <rajatsandeepsen1839@gmail.com>
2022-11-12 16:46:44 +0530

    cdn probs

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
-import { getFirestore, getDocs, addDoc, collection, orderBy, query } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
-import { getStorage, ref, uploadBytes  } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-storage.js";
+import { getFirestore, getDocs, addDoc, collection } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
+// import {  } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-storage.js";
+import {  getStorage, ref, uploadBytes } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js'
 
 const firebaseConfig = {
   apiKey: "AIzaSyCNbmkHVo6YAOk69h9OgMGbQJBUlW5xz4c",
   authDomain: "iedc-admin.firebaseapp.com",
+  // databaseURL: "https://DATABASE_NAME.firebaseio.com",
   projectId: "iedc-admin",
   storageBucket: "iedc-admin.appspot.com",
   messagingSenderId: "200933316108",
@@ -14,8 +16,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const storage = getStorage(app);
-const storageRef = ref(storage, 'some-child');
+const picStorage = getStorage(app);
+const storageRef = ref(picStorage, 'some-child');
 
 var currentdate = new Date().toLocaleString();
 
