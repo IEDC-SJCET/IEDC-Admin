0520f6ccfba01c7e0ccb2717d466e19fb44e28af
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2023-09-26 01:21:11 +0530

    feat: SIH responce table

--- src/script/startup.js
+++ src/script/startup.js
@@ -2,11 +2,42 @@ import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
 import { DB } from "./login.js";
 import { timeDifference, timeDifferenceFuture } from "./main.js";
 
+const firestoreName = "smart-india-hackathon"
+    /**
+     * @typedef member
+     * ​@property {string} branch
+     * ​@property {string} ​​name
+     * ​@property {string} ​​year 
+     * 
+     */
+
+    /**
+     * @typedef firestoredata
+     * ​@property {string} member1Email
+     * ​@property {string} member1Phone
+     * 
+     * ​@property {member} member1
+     * ​@property {member} member2
+     * ​@property {member} member3
+     * ​@property {member} member4
+     * ​@property {member} member5
+     * ​@property {member} member6
+     * 
+     * ​@property {string} describe
+     * ​@property {string} psCode
+     * ​@property {string} psTitle
+     * ​@property {string} techStack
+     * ​@property {string} theme
+     * ​@property {string} url
+     * ​@property {string} useCase
+     * ​@property {string} dependency
+     */
+
 
 export const startupform =  async () => {
     
-    const FORMS = collection(DB,'execom2023');
-    const qry = query(FORMS, orderBy("UploadTimeStamp", "desc"));
+    const FORMS = collection(DB, firestoreName);
+    const qry = query(FORMS) //, orderBy("UploadTimeStamp", "desc"));
     const responcesTableBody = document.getElementById('responcesTableBody');
 
     callmeRightNow(qry, responcesTableBody);
@@ -19,52 +50,71 @@ export const startupform =  async () => {
 
 }
 
-
-
 // iife i think
 async function callmeRightNow (qry, responcesTableBody) {
     responcesTableBody.innerHTML = ``;
     const formResponces = await getDocs(qry);
     let counter = 0;
+    
+
     formResponces.forEach((doc) => {
-        let resData = doc.data()
+        let /** @type {firestoredata}*/ resData = doc.data()
         responcesTableBody.innerHTML += bodyTemplate(resData)
         counter++;
     })
     document.getElementById('noRes').innerHTML = counter;
 }
 
-
+/**
+ * 
+ * @param {firestoredata} data 
+ * @returns {string}
+ */
 
 function bodyTemplate (data){
 
-
+    // <td class="modeField2">${choiceSpan(data.teamSelected)}</td>
     let template = `<tr>
-                    <td>${timeDifference(new Date(), new Date(data.UploadTimeStamp))}</td>
-                    <td>${data.studentName}</td>
-                    <td>${data.studentEmail}</td>
-                    <td>${data.studentPhone}</td>
+                    <td>${ data?.UploadTimeStamp ? timeDifference(new Date(), new Date(data.UploadTimeStamp)) : "-"}</td>
+                    <td>${data.member1.name}</td>
+                    <td>${data.member1.branch}</td>
+                    <td>${data.member1.year}</td>
 
-                    <td>${data.modeOfAttending}</td>
-                    <td>${data.fieldOfStudy}</td>
-                    <td>${data.currentYear}</td>
+                    <td>${data.member1Email || ""}</td>
+                    <td>${data.member1Phone || ""}</td>
 
-                    
-                    <td class="modeField2">${choiceSpan(data.teamSelected)}</td>
-                    <td class="moreField">${data.wish}</td>
-                    <td class="moreField">${data.experience}</td>
-                    <td class="moreField">${data.change}</td>
-                    <td class="moreField">${data.skills}</td>
-                    
-                    
+                    <td>${data.member2.name || ""}</td>
+                    <td>${data.member2.branch || ""}</td>
+                    <td>${data.member2.year || ""}</td>
 
+                    <td>${data.member3.name || ""}</td>
+                    <td>${data.member3.branch || ""}</td>
+                    <td>${data.member3.year || ""}</td>
+
+                    <td>${data.member4.name || ""}</td>
+                    <td>${data.member4.branch || ""}</td>
+                    <td>${data.member4.year || ""}</td>
+
+                    <td>${data.member5.name || ""}</td>
+                    <td>${data.member5.branch || ""}</td>
+                    <td>${data.member5.year || ""}</td>
+
+                    <td>${data.member6.name || ""}</td>
+                    <td>${data.member6.branch || ""}</td>
+                    <td>${data.member6.year || ""}</td>
+                    
+                    
+                    <td class="moreField">${data.describe || ""}</td>
+                    <td class="moreField">${data.psTitle || ""}</td>
+                    <td class="moreField">${data.psCode || ""}</td>
+                    <td class="moreField">${data.dependency || ""}</td>
+                    <td class="moreField">${data.techStack || ""}</td>
+                    <td class="moreField">${data.theme || ""}</td>
+                    <td class="moreField">${data.useCase || ""}</td>
+                    
                     <td>
-                        <a class="btn btn-dark rounded" target="_blank" href="${convertToURL(data.linkedin)}"><i class="bi bi-eye-fill"></i></a>
-                        <span class="invisi">${convertToURL(data.linkedin)}</span>
-                    </td>
-                    <td>
-                        <a class="btn btn-dark rounded" target="_blank" href="${convertToURL(data.portfolio)}"><i class="bi bi-eye-fill"></i></a>
-                        <span class="invisi">${convertToURL(data.portfolio)}</span>
+                        <a class="btn btn-dark rounded" target="_blank" href="${convertToURL(data.url)}"><i class="bi bi-eye-fill"></i></a>
+                        <span class="invisi">${convertToURL(data.url)}</span>
                     </td>
 
                     </tr>`
